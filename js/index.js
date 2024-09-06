2c4924cc389cea08eb19c098bd3ae1e2be1e2e4d
Mence <lucas21021996@gmail.com>
2024-09-06 12:25:31 -0300

    Added initial files;

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function changeTab(evt, tabname) 
+{
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) 
+    {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) 
+    {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabname).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    return true;
+} 
+
+function openDefaultTab()
+{
+    document.getElementById("btn-default-tab").click();
+    return true;
+}
+
