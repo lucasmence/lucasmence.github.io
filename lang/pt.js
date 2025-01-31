721ef4bc1ca9050fec6b190595442434314af864
Mence <lucas21021996@gmail.com>
2025-01-31 17:58:11 -0300

    Added portuguese language for single page translation (incomplete);

---
+++ lang/pt.js
@@ -0,0 +1,85 @@
+const language = 
+{
+    "menu":
+    {
+        "summary": "Summary",
+        "about-me": "More about me",
+        "projects": "Projects",
+        "blog": "Blog",
+        "friends": "Friends",
+        "resume": "Resume",
+        "social-media": "Social Media"
+    },
+    "summary": 
+    {
+        "welcome": "Bem-vindo(a)!",
+        "introduction": "Meu nome é Mence, eu trabalho com desenvolvimento backend, devops e gamedev.",
+
+        "bio": "Eu descobri meu gosto por programação aos dez anos quando editava e "+
+               "tentava criar os meus próprios mapas personalizados de jogos como Warcraft III e Unreal Tournament.<br/><br/> "+
+               "Desde então, sempre estive envolvido com computadores e programação "+
+               "tentando colocar as minhas ideias e conceitos da minha mente para o digital.",
+
+        "info": "Sempre me encontrará trabalhando em algum projeto pessoal, começando "+
+                "com a ideia ou arrumando os já existentes. "+
+                "Eu sempre estou trabalhando em novos projetos no tempo livre (geralmente jogos), "+
+                "gosto de automatizar tarefas manuais de servidores com bash no linux "+
+                "e pensar em novos feitiços para RPG. As ideias nunca de fluir.",
+
+        "footer": 'Você pode checar meus <a onclick="changeTab(event, \'projects\')" href="#">projetos</a> ou entrar em '+
+                  '<a href="https://linktr.ee/lucasmence">contato</a> comigo se desejar!',
+    },
+
+    "about-me":
+    {
+        "title": "Um pouco mais sobre mim",
+        "description": '',
+    },
+
+    "projects":
+    {
+        "title": "Projetos",
+
+        "boltcraft-ii-title": "Boltcraft II (2022 - 2024)",
+        "boltcraft-ii-description": '',
+
+        "helper-title": "Helper (2024 - Presente)",
+        "helper-description": '',
+
+        "boltcraft-title": "",
+        "boltcraft-description": "",
+
+        "realm-editor-title": "",
+        "realm-editor-description": "",
+
+        "lamina-title": "",
+        "lamina-description": "",
+
+        "mge-title": "",
+        "mge-description": "",
+
+        "python-game-title": "",
+        "python-game-description": "",
+
+        "pitchula-title": "",
+        "pitchula-description": "",
+    },
+
+    "blog":
+    {
+        "blog-title": "Blog",
+    },
+
+    "friends":
+    {
+        "friends-title": "",
+        "felix-title": "CSFelix",
+        "felix-description": "",
+    },   
+
+    "footer":
+    {
+        "text": "2025, Feito com ❤️ por Mence."
+    },
+    
+};
